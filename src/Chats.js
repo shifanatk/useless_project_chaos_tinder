3b2eeb6f289a91a8b3becdf46d4550d74ce06c33
Literallynihal <muhammednihalnoushad3069@gmail.com>
2026-09-12 08:20:27 +0530

    Update chat entries with new names and messages

--- src/Chats.js
+++ src/Chats.js
@@ -6,28 +6,34 @@ function Chats() {
   return (
     <div className='chats'>
         <Chat
-            name='Lisa'
-            message='Yo whats up!'
+            name='Parama chundari'
+            message='Ente peru Thekkepattu Sundari Damodharan Pillai!'
             timestamp='40 seconds ago'
-            profilePic='https://staticg.sportskeeda.com/editor/2021/10/8cd36-16334407189532-800.jpg'
+            profilePic='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_wgCw6yN0FS8DTtbx9yFLVz-hWtHPcHQhtEhXpLkFzg&s=10'
         />
         <Chat
-            name="Ellen"
-            message="Whats up❤️?"
+            name="Joey"
+            message="Whatcha doin"
             timestamp="55 minutes ago"
-            profilePic="https://imageio.forbes.com/specials-images/imageserve/5ed560d07fe4060006bbce1e/0x0.jpg?format=jpg&crop=878,879,x422,y0,safe&height=416&width=416&fit=bounds"
+            profilePic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThCT1WiF8H04-4cYM-I0nZPysiI-VZJMS_6ESlU7BpNg&s=10"
         />
         <Chat
-            name="Sandra"
-            message="Ola!"
+            name="Billy"
+            message="Oii!"
             timestamp="3 days ago"
-            profilePic="https://hips.hearstapps.com/hmg-prod/images/sandra-dee-gettyimages-154051093.jpg?crop=1xw:1.0xh;center,top&resize=640:*"
+            profilePic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdmRNayvR86eH4SaRMmsOjad62bupXSfXf-tYMidEsZA&s=10"
         />
         <Chat
-            name="Natasha"
-            message="Oops there is he is...🐶"
+            name="Muthu pandi"
+            message="hey Chello"
             timestamp="1 week ago"
-            profilePic="https://m.media-amazon.com/images/M/MV5BMzc5ZjkxYjEtODUyYS00YTA2LTk4MTItOGQyYThlMWM4ZDQ1XkEyXkFqcGdeQXVyMTE5NjI0MzYz._V1_.jpg"
+            profilePic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsfa2JHG__08oGKUA0W_k8go19ZW-Zihmv7TFxInQ4zg&s=10"
+        />
+        <Chat
+            name='Amaran'
+            message='Hey mammooty!'
+            timestamp='40 seconds ago'
+            profilePic='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO_O0DKReDVnHmVb4hvRt3MqolTNoJqYByG2VTFp4gsSG3PQ9sfFXRabY&s=10'
         />
     </div>
   )
